4a81a228752f292605ee181db8544295f6a649af
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-06-19 17:08:43 -0400

    Finished Hard Mode

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const loremIpsum = require('lorem-ipsum');
 
-app.get('/lorem', function(req, res){
-  res.sendFile('main.html', {root: __dirname});
-  res.send(loremIpsum( {count: 3, units: 'paragraphs', format: 'html'} ));
+// app.get('/lorem', function(req, res){
+//   res.sendFile('main.html', {root: __dirname});
+//   res.send(loremIpsum( {count: 3, units: 'paragraphs', format: 'html'} ));
+// });
+
+app.get('/home/:number', function(req, res){
+  let outPut = loremIpsum({
+    count:req.params.number,
+    units: 'paragraph', format: 'html'
+  });
+  res.send(outPut);
 });
 
 app.listen(3000);
